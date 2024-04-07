aff9bedf727f711aa50b016ecd96a5631b51555f
hilaEzraMoveo <hilae@moveo.co.il>
2024-04-07 12:54:05 +0300

    change to SSR

--- pages/dishes.tsx
+++ pages/dishes.tsx
@@ -5,27 +5,63 @@ import { useEffect, useState } from "react";
 import { getDishes } from "@/services/dish.service";
 import { IDish } from "@/models/dish.model";
 
-const dishes = () => {
-  const [dishes, setDishes] = useState<IDish[]>([]);
-
-  useEffect(() => {
-    fetchDishesData();
-  }, []);
-
-  const fetchDishesData = async () => {
-    try {
-      const fetchedDishes = await getDishes();
-      setDishes(fetchedDishes);
-    } catch (error) {
-      console.error("Error fetching dishes:", error);
-    }
-  };
-
+const Dishes = ({ dishesData }: { dishesData: IDish[] }) => {
   return (
     <div className="container">
       <Sidebar />
-      <GeneralTable data={dishes} columns={DishColumns} />
+      <GeneralTable data={dishesData} columns={DishColumns} />
     </div>
   );
 };
-export default dishes;
+
+export default Dishes;
+
+export async function getServerSideProps() {
+  try {
+    const fetchedDishes = await getDishes();
+    return {
+      props: {
+        dishesData: fetchedDishes,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching dishes:", error);
+    return {
+      props: {
+        dishesData: [],
+      },
+    };
+  }
+}
+
+// import Sidebar from "@/shared/components/Sidebar/Sidebar";
+// import GeneralTable from "../shared/components/GeneralTable/GeneralTable";
+// import { DishColumns } from "@/data/tableColumns.data";
+// import { useEffect, useState } from "react";
+// import { getDishes } from "@/services/dish.service";
+// import { IDish } from "@/models/dish.model";
+
+// const dishes = () => {
+//   const [dishes, setDishes] = useState<IDish[]>([]);
+
+//   useEffect(() => {
+//     fetchDishesData();
+//   }, []);
+
+//   const fetchDishesData = async () => {
+//     try {
+//       const fetchedDishes = await getDishes();
+//       setDishes(fetchedDishes);
+//     } catch (error) {
+//       console.error("Error fetching dishes:", error);
+//     }
+//   };
+
+//   return (
+//     <div className="container">
+//       <Sidebar />
+//       <GeneralTable data={dishes} columns={DishColumns} />
+//     </div>
+//   );
+// };
+// export default dishes;
